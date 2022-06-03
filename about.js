4390f0730b883ac87d9c737b9250999e63cfc60a
Didier Dakoury <dazibao76@gmail.com>
2022-06-03 11:00:42 +0000

    first commit

---
+++ about.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+// let parentElem = document.getElementById('about_text');
+// let childElem = document.getElementById('about_description');
+// parentElem.removeChild(childElem);
+
+
+//  // Setting inner HTML contents
+// let textDiv = document.getElementById('about_text');
+// textDiv.innerHTML = '<div><span>Web developer, with extensive</span><br><span>knowledge and years of </span> <br><span> experience, working in web</span><br><span>technologies and Ui / Ux design, </span><br><span> delivering quality work</span></div>'
+
+
+
